60c94913ddabda06b2394f3c98a0b4beca8b89c5
Kriszhu <m13218962398@163.com>
2016-12-21 09:28:17 +0800

    test
    test

---
+++ game/testTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  testTableViewCell.h
+//  game
+//
+//  Created by wanrun on 2016/12/21.
+//  Copyright © 2016年 wanrun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testTableViewCell : UITableViewCell
+
+@end
---
+++ game/testTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  testTableViewCell.m
+//  game
+//
+//  Created by wanrun on 2016/12/21.
+//  Copyright © 2016年 wanrun. All rights reserved.
+//
+
+#import "testTableViewCell.h"
+
+@implementation testTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
